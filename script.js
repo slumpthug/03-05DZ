e805b87b6fae175e30987dda316db391a4884e39
slumpthug <gangguchi58@gmail.com>
2022-07-08 21:04:35 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+//задание 1
+// const salary = {
+//     Kola: 1000,
+//     Petya: 2300,
+//     Vasya: 2000,
+//     Anna: 800,
+//     Victoria: 1500,
+// };
+
+// console.log('Зарплата ' + salary.Kola);
+
+// console.log('Зарплата ' + salary.Petya);
+
+//задание 2
+// const salary = {
+//     Kola: 1000,
+//     Petya: 2300,
+//     Vasya: 2000,
+//     Anna: 800,
+//     Victoria: 1500,
+// };
+
+// salary.John = 2100;
+
+// delete salary.Vasya
+
+// console.log(salary);
+
+//задание 3
+// const salary = {
+//     Kola: 1000,
+//     Petya: 2300,
+//     Vasya: 2000,
+//     Anna: 800,
+//     Victoria: 1500,
+// };
+
+// console.log('Зарплата ' + salary["Kola"]);
+
+// console.log('Зарплата ' + salary["Petya"]);
+
+//задание 4
+// const cat = {
+//     name: 'Barsik',
+//     eyeColor: 'Blue',
+//     woolColor: 'Grey',
+//     weight: '1kg',
+//     satiety: false,
+// };
+
+// console.log(cat);
+
+// if (cat.satiety === false) {
+//     console.log('Кис кис ' + cat.name + ' идем кушать');
+//     cat.satiety = true
+// } else {
+//     console.log('Значит сытый');
+// };
